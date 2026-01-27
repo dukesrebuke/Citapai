1aa9c34a90d8e490ff3acfb4c7e863a99ea40eed
dukesrebuke <82522053+dukesrebuke@users.noreply.github.com>
2026-01-27 09:34:45 -0500

    Increase temperature parameter to 1.1

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -33,7 +33,7 @@ exports.handler = async (event) => {
             content: userQuery
           }
         ],
-        temperature: 0.1,
+        temperature: 1.1,
         max_tokens: 1024
       })
     });
